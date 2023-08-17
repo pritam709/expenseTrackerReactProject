e262818443423e40f3bef38976f5b45a2be06efa
Pravesh <pritamyadav70923@gmail.com>
2023-08-17 23:09:00 +0530

    firebase backend

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,32 +1,67 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import classes from "./Home.module.css";
 import { Link } from "react-router-dom";
 import ExpenseForm from "../components/ExpenseForm";
 import Expenses from "../components/Expenses";
 const Home = () => {
-  const[expenses,setExpenses]=  useState([])
+  const [expenses, setExpenses] = useState([]);
+  useEffect(() => {
+    const getExpense = async () => {
+      const response = await fetch(
+        "https://expensetracker-d0652-default-rtdb.firebaseio.com/expense.json"
+      );
+
+      const resData =await response.json();
+      console.log(resData);
+      const fetchResult=[];
+      for(let key in resData){
+        fetchResult.unshift(
+            {
+                ...resData[key],
+                id:key
+            }
+        )
+      }
+      console.log(fetchResult);
+      setExpenses(fetchResult);
+     
+    };
+    getExpense();
+  }, []);
+
   const logoutHandler = () => {
     localStorage.removeItem("token");
     localStorage.removeItem("email");
     console.log(localStorage.getItem("token"));
   };
 
-  const addExpenseHandler=(obj)=>{
+  const addExpenseHandler = (obj) => {
     console.log(obj);
-        setExpenses(prevState=>{
-        return [...prevState,obj]
-    })
-  }
+    setExpenses((prevState) => {
+      return [...prevState, obj];
+    });
+    fetch(
+      "https://expensetracker-d0652-default-rtdb.firebaseio.com/expense.json",
+      {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(obj),
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
   return (
     <>
-    <Link to="/">
-          {" "}
-          <button className={classes.logBtn} onClick={logoutHandler}>
-            Logout
-          </button>
-        </Link>
+      <Link to="/">
+        {" "}
+        <button className={classes.logBtn} onClick={logoutHandler}>
+          Logout
+        </button>
+      </Link>
       <div className={classes.header}>
-        
         <span>
           <i>Welcome to Expense Tracker!!!!</i>
         </span>{" "}
@@ -38,7 +73,6 @@ const Home = () => {
       <ExpenseForm onSaveExpenseData={addExpenseHandler} />
       <Expenses items={expenses} />
     </>
-   
   );
 };
 export default Home;
